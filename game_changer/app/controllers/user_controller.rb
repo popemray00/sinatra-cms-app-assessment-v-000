6efcd298c1e4f271f9740eaedfe791c748683e45
Michael Pope <popemray1986@gmail.com>
2018-07-20 12:16:49 +0000

    automatically backed up by learn

--- game_changer/app/controllers/user_controller.rb
+++ game_changer/app/controllers/user_controller.rb
@@ -26,14 +26,13 @@ class UserController < ApplicationController
         session[:user_id] = @user.id
         redirect to '/games'
       else
-        flash[:message] = "Username is already taken, please choose another name."
         redirect to '/login'
       end
       end
     end
 
     get '/login' do
-      flash[:message] = "Please sign up before you sign in"
+      flash[:message] = "Username is already taken, please choose another name."
       if !logged_in?
         erb :'users/login'
       else
