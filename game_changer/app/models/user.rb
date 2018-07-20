3602e8d6508fb206b1beaed7431cdb7890b0b004
Michael Pope <popemray1986@gmail.com>
2018-07-20 11:40:08 +0000

    automatically backed up by learn

--- game_changer/app/models/user.rb
+++ game_changer/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :games
   has_secure_password
-  validates :username, presence => true, uniqueness => true
+  validates :username, :presence => true, :uniqueness => true
 
   def slug
     username.downcase.gsub(" ","-")
